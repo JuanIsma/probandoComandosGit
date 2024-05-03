c5072402a1ab87da297fcc30aa75ceebab508ccb
JuanIsma <juanflatron@gmail.com>
2024-05-03 00:37:35 -0300

    Prueba master

--- index.js
+++ index.js
@@ -66,3 +66,35 @@ const imprimirAlert= mensaje => alert(mensaje='Hi!');
  saludar2("jony" , imprimirAlert);
  console.log(imprimirConsola);
  console.log(imprimirAlerta);
+
+
+
+
+
+
+
+
+
+ function cargar(sueldos) {
+    for (let f = 0; f < sueldos.length; f++) {
+        let v = prompt('Ingrese sueldo:');
+        sueldos[f] = parseInt(v);
+    }
+}
+
+function calcularGastos(sueldos) {
+    let total = 0;
+    for (let f = 0; f < sueldos.length; f++) {
+        total = total + sueldos[f];
+    }
+    document.write('Listado de sueldos<br>');
+    for (let f = 0; f < sueldos.length; f++) {
+        document.write(sueldos[f] + '<br>');
+    }
+    document.write('Total de gastos en sueldos:' + total);
+}
+
+let sueldos;
+sueldos = new Array(2);
+cargar(sueldos);
+calcularGastos(sueldos);
